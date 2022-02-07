3cddd71ee74b90208d6991377bb8537c346489d4
gromozek <sva1973@yandex.ru>
2022-02-07 08:56:36 +0000

    fixed some bugs

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -28,12 +28,12 @@ resource "null_resource" "sync" {
   ]
 }
 
-#resource "null_resource" "monitoring" {
-#  provisioner "local-exec" {
-#    command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook -i ../ansible/inventory ../ansible/swarm-deploy-stack.yml --limit=managers"
-#  }
-#
-#  depends_on = [
-#    null_resource.sync
-#  ]
-#}
+resource "null_resource" "monitoring" {
+  provisioner "local-exec" {
+    command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook -i ../ansible/inventory ../ansible/swarm-deploy-stack.yml --limit=managers"
+  }
+
+  depends_on = [
+    null_resource.sync
+  ]
+}
